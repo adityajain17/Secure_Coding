c265c48da9d6aff1af9e4f5d5bc3c8fda45f10ba
adtjn9 <32116440+adtjn9@users.noreply.github.com>
2019-02-11 19:32:47 +0530

    Delete Secure_Lab_Direcotry_Code.py

--- Secure_Lab_Direcotry_Code.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-subprocess.call('mkdir Adi',shell=True)
-i=0
-for i in range(10):
-    subprocess.call('mkdir C:\\Users\\User\\Desktop\\Adi\\SubFolder'+str(i+1),shell=True)
-    j=0
-    for j in range(10):
-        subprocess.call('abc >C:\\Users\\User\\Desktop\\Adi\\SubFolder'+str(i+1)+str('/17BCE7066')+str(i*10+j+1)+'.txt',shell=True)
